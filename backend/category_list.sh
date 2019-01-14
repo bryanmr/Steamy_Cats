fa2db7a6fb22bc18f78675d2176667d844d17e66
Bryan Reese <bryan.mreese@gmail.com>
2019-01-14 13:13:44 -0500

    Did a code refactor so things are in one folder Also started working on ProtonDB ratings

--- backend/category_list.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mkdir -p ~/.config/Steamy_Cats/
-echo "# List of all categories that games are assigned"
-echo "# To skip over unwanted categories, save category names to ~/.config/Steamy_Cats/exclude.conf"
-echo -n "# Lines with comments will be ignored."
-grep -h -A100 \"tags\" /var/tmp/steamy_cats/* | grep -v -e "}" -e "{" -e "^--" | cut -d\" -f4 | sort -u
